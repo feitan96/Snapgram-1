07fbeb02b58d09cac8b90396476537782c6a0ffd
feitan96 <113403122+feitan96@users.noreply.github.com>
2024-03-07 23:05:22 +0800

    account creation commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
+import { Toaster } from "@/components/ui/toaster"
 
 
 const App = () => {
@@ -23,6 +24,8 @@ const App = () => {
           <Route index element={<Home />} />     
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   )
 }
