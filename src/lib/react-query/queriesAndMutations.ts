33d72c0ee5a96c7ee404d611df0b3df909144f37
feitan96 <113403122+feitan96@users.noreply.github.com>
2024-03-08 01:38:13 +0800

    topbar ui unfinished

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,7 +5,7 @@ import {
     useQueryClient,
     useInfiniteQuery
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -20,3 +20,9 @@ export const useSignInAccount = () => {
         }) => signInAccount(user)
     })
 }
+
+export const useSignOutAccount = () => {
+    return useMutation({
+      mutationFn: signOutAccount,
+    });
+  };
