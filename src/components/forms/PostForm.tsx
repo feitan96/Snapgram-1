01873d82e66f1cc81a563ea21628516357acbe05
feitan96 <113403122+feitan96@users.noreply.github.com>
2024-03-10 16:25:53 +0800

    Homepage layout

---
+++ src/components/forms/PostForm.tsx
@@ -0,0 +1,14 @@
+import * as z from "zod";
+import { Models } from "appwrite";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const PostForm = () => {
+  return (
+    <div>PostForm</div>
+  )
+}
+
+export default PostForm
