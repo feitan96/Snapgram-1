3e768341054c53910b36355b325c829874f2edc4
feitan96 <113403122+feitan96@users.noreply.github.com>
2024-03-11 22:48:48 +0800

    create post message

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,13 +1,131 @@
-import * as z from "zod";
-import { Models } from "appwrite";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "../ui/textarea"
+import FileUploader from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { Models } from "appwrite"
 
-const PostForm = () => {
+type PostFormProps = {
+    post?: Models.Document;
+    action: "Create" | "Update";
+  };
+  
+  const PostForm = ({ post, action }: PostFormProps) => {
+    const form = useForm<z.infer<typeof PostValidation>>({
+        resolver: zodResolver(PostValidation),
+        defaultValues: {
+            caption: post ? post?.caption : "",
+            file: [],
+            location: post ? post.location : "",
+            tags: post ? post.tags.join(",") : "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof PostValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>PostForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} 
+      className="flex flex-col gap-9 w-full  max-w-5xl">
+        <FormField
+          control={form.control}
+          name="caption"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Caption</FormLabel>
+              <FormControl>
+                <Textarea 
+                    className="shad-textarea custom-scrollbar" 
+                    placeholder="type here" 
+                    {...field} 
+                />
+              </FormControl>
+              <FormDescription>
+                What is your post about?
+              </FormDescription>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Photos</FormLabel>
+              <FormControl>
+                <FileUploader
+                    fieldChange={field.onChange}
+                    mediaUrl={post?.imageUrl}
+                />
+              </FormControl>
+              <FormDescription>
+                What is your post about?
+              </FormDescription>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Location</FormLabel>
+              <FormControl>
+                <Input className="shad-input" placeholder="Location" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Tags (separated by comma " , ")</FormLabel>
+              <FormControl>
+                <Input 
+                    type="text"
+                    className="shad-input" 
+                    placeholder="Nigga, Black, Monke" {...field} 
+                />
+              </FormControl>
+              <FormDescription>
+                What is your post about?
+              </FormDescription>
+              <FormMessage className="shad-form_message"/>
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-4 items-center justify-end">
+            <Button 
+                type="button" 
+                className="shad-button_dark_4"
+            >
+                Cancel
+            </Button>
+            <Button 
+                type="submit" 
+                className="shad-button_primary whitespace-nowrap"
+            >
+                Submit
+            </Button>
+        </div>
+      </form>
+    </Form>
   )
 }
 
